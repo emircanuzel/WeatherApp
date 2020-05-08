207a9e15127a6b43255a0cc5e923174472bed7b5
Emircan UZEL <emircan.uzel@vektortelekom.com>
2020-05-08 17:43:01 +0300

    initial first commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Emircan UZEL on 8.05.2020.
+//  Copyright © 2020 Emircan UZEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
